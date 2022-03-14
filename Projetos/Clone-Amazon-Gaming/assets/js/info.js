59acc18c8f4e31ef3dd3ecc57cdc9d8c131c13ab
Lucas Gomes de Oliveira <lucasgabrielgomes33@gmail.com>
2022-03-14 16:32:25 -0300

    patch 1.2

---
+++ Projetos/Clone-Amazon-Gaming/assets/js/info.js
@@ -0,0 +1,24 @@
+let info = [
+     {
+          id:1, 
+          p1:' Jogos grátis incluídos com Prime, seus para guardar sempre', 
+          p2:' Suba de nível com armas, personagens, skins, potencializadores, acessórios e muito mais',
+     },
+     {
+          id:2, 
+          p1:' Frete ilimitado em milhões de produtos sem valor mínimo de compra', 
+          p2:' Acesso antecipado a Ofertas Relâmpago em Games e Consoles, Eletrônicos, exclusivas e &nbsp&nbsp&nbsp&nbsp&nbsp&nbspprevidenciárias',
+     },
+     {
+          id:3, 
+          p1:' Assista em qualquer lugar com seu console, smartphone, smart TV e muito mais', 
+          p2:' Streaming ilimitado em qualidade HD',
+          p3:' O único lugar para assistir Tom Clancy s Jack Ryan, The Grand Tour, The Tick, e mais',
+     },
+     {
+          id:3, 
+          p1:' Use sua inscrição no canal (valor de US $4) todos os meses para 9 seus streamers favoritos', 
+          p2:' Converse no chat usando o emblema exclusivo para assinantes',
+          p3:' Cores e emoticons exclusivos de bate-papo da Twitch',
+     }
+]
---
+++ Projetos/Clone-Amazon-Gaming/assets/js/script.js
@@ -0,0 +1,71 @@
+let paragrafo = document.querySelector('.menu-pcl-a-2 .description');
+let imagem = document.querySelector('.menu-pcl--area-right');
+let button1 = document.querySelector('.menu-pcl--conteudo .box1');
+let button2 = document.querySelector('.menu-pcl--conteudo .box2');
+let button3 = document.querySelector('.menu-pcl--conteudo .box3');
+let button4 = document.querySelector('.menu-pcl--conteudo .box4');
+
+
+function trocar(n) {
+     if(n == '1') {
+          imagem.style.backgroundImage = 'url(assets/img/ilustracao-1.png)';
+          if(button1.classList.contains('btn-selected') == false){
+               clear();
+
+               layout1 = '';
+               layout1 += `<p>${info[0].p1}</p>`;
+               layout1 += `<p>${info[0].p2}</p>`;
+          
+               button1.classList.add('btn-selected');
+               paragrafo.innerHTML = layout1;
+          }
+     }
+     else if(n == '2') {
+          imagem.style.backgroundImage = 'url(assets/img/ilustracao-2.png)';
+          if(button2.classList.contains('btn-selected') == false){
+               clear();
+               
+               layout2 = '';
+                    layout2 += `<p>${info[1].p1}</p>`;
+                    layout2 += `<p>${info[1].p2}</p>`;
+               
+               button2.classList.add('btn-selected');
+               paragrafo.innerHTML = layout2;
+          }
+     }
+     else if(n == '3') {
+          imagem.style.backgroundImage = 'url(assets/img/ilustracao-3.png)';
+          if(button3.classList.contains('btn-selected') == false){
+               clear();
+
+               layout3 = '';
+               layout3 += `<p>${info[2].p1}</p>`;
+               layout3 += `<p>${info[2].p2}</p>`;
+               layout3 += `<p>${info[2].p3}</p>`;
+          
+               button3.classList.add('btn-selected');
+               paragrafo.innerHTML = layout3;
+          }
+     }
+     else if(n == '4') {
+          imagem.style.backgroundImage = 'url(assets/img/ilustracao-4.png)';
+          if(button4.classList.contains('btn-selected') == false){
+               clear();
+
+               layout4 = '';
+               layout4 += `<p>${info[3].p1}</p>`;
+               layout4 += `<p>${info[3].p2}</p>`;
+               layout4 += `<p>${info[3].p3}</p>`;
+
+               button4.classList.add('btn-selected');
+               paragrafo.innerHTML = layout4;
+          }
+     }
+}
+
+function clear() {
+     button1.classList.remove('btn-selected');
+     button2.classList.remove('btn-selected');
+     button3.classList.remove('btn-selected');
+     button4.classList.remove('btn-selected');
+}
